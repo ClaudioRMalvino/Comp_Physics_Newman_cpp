6e0569bfc664d34a2e6f854d52befeb04206dc66
ClaudioRMalvino <claudiormal@gmail.com>
2024-11-04 21:57:53 -0500

    adding completed exercise 2.11 part c) plus some code changes for readability

--- chapter_2/exercise_2_11c.cpp
+++ chapter_2/exercise_2_11c.cpp
@@ -5,25 +5,37 @@
 * From: Computational Physics by Mark Newman
 */
 
-#include <boost/multiprecision/cpp_int.hpp>
-#include <boose/multiprecision/detail/integer_ops.hpp>
+
+#include <cmath>
+#include <iomanip>
 #include <iostream>
-#include <range>
+#include <ranges>
 #include <string>
 #include <vector>
+
 
 //Function Declaration
 
 /**
+ * @brief Function takes an vector array of double type values and calculates
+ * the sum of all values in the vector array.
+ *
+ * @param arr: An vector array of doubles
+ *
+ * @return The sum of all elements in the vector array
+ */
+
+double sum(std::vector<double> &arr);
+
+/**
  * @brief Calculates the factorial of an integer
  *
- * @params:
- *  x: The integer to calculate factorial for
+ * @params x: The integer to calculate factorial for
  *
  * @return
  *  x!
  */
-boost::multiprecision::cpp_int factorial(int x);
+double factorial(int x);
 
 /**
  * @brief Calculates the binomial coefficient (n choose k)
@@ -33,7 +45,206 @@ boost::multiprecision::cpp_int factorial(int x);
  *
  * @return The bionomial coefficient (n choose k)
  */
-boost::multiprecision::cpp_int binomial_coefficient(int n, int k);
+double binomial_coefficient(int n, int k);
+
+/**
+ * @brief Function calculates the probability that an unbiased coin tossed
+ * n times wil come up heads or tails k times
+ *
+ * @param num_tosses: The number of simulated tosses of the coin
+ * @param target: The number of times the coin will come up heads or tails
+ *
+ * @return Probability of getting heads or tails K or more times
+ */
+double unbiased_coin_toss(int num_tosses, int target);
+
+/**
+ * @brief Function calculates the probability that it comes up heads or tails
+ * K or more times
+ *
+ * @param num_tosses: The number of simulated tosses of the coin
+ * @param target: The number of times the coin will come up head/tails K num
+ * of times
+ *
+ * @return Probability of getting heads or tails K or more times
+ */
+double prob_k_or_more(int num_tosses, int target);
+
+/**
+ * @brief Prompts for and reads the value of total number of
+ * tosses from user input
+ *
+ * @return The input string (which may be a number or 'q' to quit)
+ */
+std::string get_num_tosses();
+
+/**
+ * @brief Prompts for and reads the value of the total number of target
+ * heads or tails from user input
+ *
+ * @return The input string (which may be a number or 'q' to quit)
+ */
+std::string get_target();
+
+/**
+ * @brief Checks if the input values num_tosses and target are valid
+ *
+ * (* - see above for parameter definition)
+ * @param num_tosses *
+ * @param target *
+ *
+ * @throws std::runtime_error if inputs are invalid
+ */
+void check_values(int num_tosses, int target);
+
+/**
+ * @brief Prints results of unbiased_coin_toss and prob_k_or_more
+ */
+void print_results(double probability1,
+                   double probability2,
+                   int num_tosses,
+                   int target);
+
+int main()
+{
+    std::cout << "Unbiased Coin Toss Simulator" << '\n'
+              << "Input 'q' to quit" << '\n';
+
+    while (true)
+    {
+        auto num_tosses_input{get_num_tosses()};
+        if (num_tosses_input == "q")
+        {
+            break;
+        }
+
+        auto target_input{get_target()};
+        if (target_input == "q")
+        {
+            break;
+        }
 
 
+        int num_tosses{std::stoi(num_tosses_input)};
+        int target{std::stoi(target_input)};
 
+        try
+        {
+            check_values(num_tosses, target);
+
+            auto prob1{unbiased_coin_toss(num_tosses, target)};
+            auto prob2{prob_k_or_more(num_tosses, target)};
+
+            print_results(prob1, prob2, num_tosses, target);
+        }
+        catch (const std::invalid_argument &e)
+        {
+            std::cerr << "Invalud input. Please try again." << '\n';
+        }
+    }
+    return 0;
+}
+
+// Function Definitions
+
+double sum(std::vector<double> &arr)
+{
+    double result = 0.0;
+    for (double num : arr)
+    {
+        result += num;
+    }
+
+    return result;
+}
+
+double factorial(int x)
+{
+    double value = 1.0;
+    for (int i : std::views::iota(1, x + 1))
+    {
+        value *= i;
+    }
+    return value;
+}
+
+double binomial_coefficient(int n, int k)
+{
+    if (k == 0)
+    {
+        return 1;
+    }
+    double numerator{factorial(n)};
+    double denominator{factorial(k) * factorial(n - k)};
+    double binomial_coef{numerator / denominator};
+
+    return binomial_coef;
+}
+
+
+double unbiased_coin_toss(int num_tosses, int target)
+{
+    double probability{binomial_coefficient(num_tosses, target) *
+                       (std::pow(0.5, num_tosses))};
+    return probability;
+}
+
+double prob_k_or_more(int num_tosses, int K)
+{
+    std::vector<double> values;
+
+    for (int i : std::views::iota(K, num_tosses + 1))
+    {
+        values.push_back(unbiased_coin_toss(num_tosses, i));
+    }
+
+    double probability = sum(values);
+
+    return probability;
+}
+
+std::string get_num_tosses()
+{
+    std::cout << "Input number of coin tosses: ";
+
+    std::string input{};
+    std::cin >> input;
+
+    return input;
+}
+
+std::string get_target()
+{
+    std::cout << "Input number of desired heads/tails: ";
+
+    std::string input{};
+    std::cin >> input;
+
+    return input;
+}
+
+void check_values(int num_tosses, int target)
+{
+    if (num_tosses <= 0)
+    {
+        throw std::runtime_error("number of tosses must be > 0");
+    }
+
+    if (target <= 0)
+    {
+        throw std::runtime_error("number of desired heads/tails must be > 0");
+    }
+}
+
+void print_results(double probability1,
+                   double probability2,
+                   int num_tosses,
+                   int target)
+{
+    std::cout << std::setprecision(3) << '\n'
+              << "Probability of " << target << " heads/tails out of "
+              << num_tosses << " tosses: " << (probability1 * 100.0) << "%";
+
+    std::cout << "Probability of getting heads/tails " << target
+              << " times or more: " << (probability2 * 100.0) << "%" << '\n';
+}
--- chapter_2/exercise_2_3.cpp
+++ chapter_2/exercise_2_3.cpp
@@ -12,69 +12,76 @@
 
 // cart_to_polar calculates the radius (r) from the origin of point (x,y)
 // and the angle theta. Returns a tuple (r, theta).
-std::tuple<double, double> cart_to_polar(double x, double y) {
+std::tuple<double, double> cart_to_polar(double x, double y)
+{
 
-  const double pi{std::acos(-1)};
+    const double pi{std::acos(-1)};
 
-  // Calculates the distance from the origin at point (x,y)
-  double r = std::sqrt(std::pow(x, 2) + std::pow(y, 2));
+    // Calculates the distance from the origin at point (x,y)
+    double r = std::sqrt(std::pow(x, 2) + std::pow(y, 2));
 
-  // Calculates the angle at point (x/y) from the axis and converts to degrees
-  double theta = std::atan(y / x) * (180.0 / pi);
+    // Calculates the angle at point (x/y) from the axis and converts to degrees
+    double theta = std::atan(y / x) * (180.0 / pi);
 
-  return std::make_tuple(r, theta);
+    return std::make_tuple(r, theta);
 }
 
 // get_x_pos asks for user input to define the x position and returns a string
-std::string get_x_pos() {
+std::string get_x_pos()
+{
 
-  std::cout << "Enter x: ";
-  std::string inputx;
-  std::cin >> inputx;
-  return inputx;
+    std::cout << "Enter x: ";
+    std::string inputx;
+    std::cin >> inputx;
+    return inputx;
 }
 // get_y_pos asks for user input to define the y position and returns a string
-std::string get_y_pos() {
+std::string get_y_pos()
+{
 
-  std::cout << "Enter y: ";
-  std::string inputy;
-  std::cin >> inputy;
-  return inputy;
+    std::cout << "Enter y: ";
+    std::string inputy;
+    std::cin >> inputy;
+    return inputy;
 }
 
-int main() {
+int main()
+{
 
-  std::cout << "Cartesian Coordinate to Polar Coordinate Converter" << '\n';
+    std::cout << "Cartesian Coordinate to Polar Coordinate Converter" << '\n';
 
-  // Loop which performs the required actions to take define (x,y) and returns
-  // values for (r, theta).
-  while (true) {
+    // Loop which performs the required actions to take define (x,y) and returns
+    // values for (r, theta).
+    while (true)
+    {
 
-    auto inputx{get_x_pos()};
-    if (inputx == "q") {
-      break;
+        auto inputx{get_x_pos()};
+        if (inputx == "q")
+        {
+            break;
+        }
+
+        auto inputy{get_y_pos()};
+        if (inputy == "q")
+        {
+            break;
+        }
+
+        try
+        {
+
+            // Converts our stings to doubles for the purpose of cart_to_polar(x,y)
+            double x = std::stod(inputx);
+            double y = std::stod(inputy);
+
+            auto [r, theta] = cart_to_polar(x, y);
+
+            std::cout << '\n' << "(r: " << r << ", theta: " << theta << ")" << '\n' << std::endl;
+        }
+        catch (const std::invalid_argument &e)
+        {
+            std::cerr << "Invalid input. Please enter numeric value" << std::endl;
+        }
     }
-
-    auto inputy{get_y_pos()};
-    if (inputy == "q") {
-      break;
-    }
-
-    try {
-
-      // Converts our stings to doubles for the purpose of cart_to_polar(x,y)
-      double x = std::stod(inputx);
-      double y = std::stod(inputy);
-
-      auto [r, theta] = cart_to_polar(x, y);
-
-      std::cout << '\n'
-                << "(r: " << r << ", theta: " << theta << ")" << '\n'
-                << std::endl;
-
-    } catch (const std::invalid_argument &e) {
-      std::cerr << "Invalid input. Please enter numeric value" << std::endl;
-    }
-  }
-  return 0;
+    return 0;
 }
